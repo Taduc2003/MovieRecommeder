dbd9e29fd685416be4ee487423bfd909a6900ae3
Taduc2003 <buitaduc139@gmail.com>
2025-12-27 01:38:48 +0700

    add test

--- src/ml/tests.py
+++ src/ml/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from ml.utils import get_data_loader
+from surprise import SVD, accuracy
 
-# Create your tests here.
+
+class DataLoaderTest(TestCase):
+    def test_data_loader_row_count(self):
+        data = [
+            {"userId": 1, "movieId": 1, "rating": 4},
+            {"userId": 2, "movieId": 2, "rating": 5},
+        ]
+        dataset = get_data_loader(data)
+        df = dataset.df
+        self.assertEqual(len(df), 2)
+
+    def test_data_loader_with_null(self):
+        data = [
+            {"userId": 1, "movieId": 1, "rating": 4},
+            {"userId": None, "movieId": 2, "rating": 5},  # userId bị null
+        ]
+        dataset = get_data_loader(data)
+        df = dataset.df
+        # Chỉ còn 1 dòng hợp lệ
+        self.assertEqual(len(df), 1)
+
+
+class ModelQualityTest(TestCase):
+    def test_rmse_threshold(self):
+        data = [
+            {"userId": 1, "movieId": 1, "rating": 4},
+            {"userId": 1, "movieId": 2, "rating": 5},
+            {"userId": 2, "movieId": 1, "rating": 3},
+            {"userId": 2, "movieId": 2, "rating": 4},
+        ]
+        dataset = get_data_loader(data)
+        trainset = dataset.build_full_trainset()
+        model = SVD(n_epochs=5)
+        model.fit(trainset)
+        predictions = model.test(trainset.build_testset())
+        rmse = accuracy.rmse(predictions, verbose=False)
+        self.assertLess(rmse, 1.0)
+
+    def test_predict_output_range(self):
+        data = [
+            {"userId": 1, "movieId": 1, "rating": 4},
+            {"userId": 2, "movieId": 2, "rating": 5},
+        ]
+        dataset = get_data_loader(data)
+        trainset = dataset.build_full_trainset()
+        model = SVD(n_epochs=2)
+        model.fit(trainset)
+        pred = model.predict(uid=1, iid=1)
+        self.assertTrue(1.0 <= pred.est <= 5.0)
--- src/suggestions/tests.py
+++ src/suggestions/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase, Client, tag
+from django.contrib.auth import get_user_model
+from movies.models import Movie
+from suggestions.models import Suggestion
+
+class SuggestionApiIntegrationTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(username='testuser', password='testpass')
+        cls.movie = Movie.objects.create(id=1, title="Movie 1")
+        Suggestion.objects.create(user=cls.user, object_id=cls.movie.id, value=4.5, content_type_id=1)
+
+    @tag('integration')
+    def test_top_n_recommendation(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get('/api/suggestions/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(len(response.json().get('suggestions', [])) > 0)
+
+    @tag('integration')
+    def test_cold_start(self):
+        new_user = get_user_model().objects.create_user(username='cold', password='cold')
+        self.client.login(username='cold', password='cold')
+        response = self.client.get('/api/suggestions/')
+        self.assertEqual(response.status_code, 200)
+        # Không được crash, có thể trả về danh sách phổ biến hoặc rỗng
